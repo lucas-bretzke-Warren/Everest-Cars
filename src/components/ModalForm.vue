fab8156b02cdb63b616d37a2d5024b307a24aea7
Lucas Bretzke <lucas.bretzke@warren.com.br>
2022-07-12 11:21:08 -0300

    feat: Base modal

---
+++ src/components/ModalForm.vue
@@ -0,0 +1,219 @@
+<template>
+  <section class="modal">
+    <nav>
+      <h4>Adicionar carro</h4>
+      <button @click="closeModal()">X</button>
+    </nav>
+    <form class="form_new_car">
+      <div class="content">
+        <label for="">Nome do carro</label>
+        <input type="text" placeholder="nome?" v-model="name" required />
+      </div>
+      <div class="content">
+        <label for="">Marca</label>
+        <input type="text" v-model="marca" placeholder="Reno,Fiat,Volvo..." />
+      </div>
+      <div class="content">
+        <label for="">Cor</label>
+        <input type="text" v-model="cor" placeholder="branco,cinza,rosa..." />
+      </div>
+      <div class="content">
+        <label for="">Ano</label>
+        <input type="text" v-model="ano" placeholder="##/##/####" />
+      </div>
+      <div class="content">
+        <label for=""> Quantidade de portas</label>
+        <input type="text" v-model="portas" placeholder="digite um numero" />
+      </div>
+      <div class="content">
+        <label for="">CV</label>
+        <input type="text" v-model="cv" placeholder="quantos CV ?" />
+      </div>
+      <div class="content">
+        <label for="">Câmbio</label>
+        <input type="text" v-model="cambio" placeholder="automático / manual" />
+      </div>
+      <div class="content">
+        <label for="">Alarme</label>
+        <input
+          type="text"
+          v-model="alarme"
+          placeholder="possui, sim / não ???"
+        />
+      </div>
+      <div class="content">
+        <label for="">Teto solar</label>
+        <input
+          type="text"
+          v-model="tetoSolar"
+          placeholder="possui, sim / não ???"
+        />
+      </div>
+      <div class="content">
+        <label for="">Computador de bordo</label>
+        <input
+          type="text"
+          v-model="compudadordeBordo"
+          placeholder="possui, sim / não ???"
+        />
+      </div>
+    </form>
+    <footer>
+      <input type="reset" value="LIMPAR" />
+      <button @click="newPost">CONCLUIR</button>
+    </footer>
+  </section>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "ModalForm",
+  data() {
+    return {
+      url: "https://my-json-server.typicode.com/willianWarren/backendAPI/cars",
+      name: "",
+      marca: "",
+      cor: "",
+      ano: "",
+      portas: "",
+      cv: "",
+      alarme: "",
+      cambio: "",
+      tetoSolar: "",
+      compudadordeBordo: "",
+    };
+  },
+  methods: {
+    closeModal() {
+      this.$emit("closeModal");
+    },
+    async newPost() {
+      try {
+        const payload = {
+          nome: "Corsa",
+          marca: "Fiat",
+          cor: "vermelho",
+          ano: "2001",
+        };
+        await axios.post(this.url, payload);
+        this.closeModal();
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+};
+</script>
+
+<style  lang="less" scoped>
+@bg-color: {
+  background-color: #cddcdc;
+  background-image: radial-gradient(
+      at 50% 100%,
+      rgba(255, 255, 255, 0.5) 0%,
+      rgba(0, 0, 0, 0.5) 100%
+    ),
+    linear-gradient(
+      to bottom,
+      rgba(255, 255, 255, 0.25) 0%,
+      rgba(0, 0, 0, 0.25) 100%
+    );
+  background-blend-mode: screen, overlay;
+};
+
+.modal {
+  @bg-color();
+  width: 550px;
+  height: 394px;
+  padding: 20px;
+  flex-direction: column;
+
+  border: none;
+  border-radius: 5px;
+
+  box-shadow: 0px 0px 1px 1100px rgba(0, 0, 0, 0.568);
+
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  right: -50%;
+  transform: translate(-50%, -50%);
+  nav {
+    width: 100%;
+    padding-bottom: 10px;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    border-bottom: 1px solid #444;
+
+    button {
+      position: absolute;
+      text-align: center;
+
+      top: -15px;
+      right: -15px;
+      font-size: 25px;
+      color: red;
+      background-color: rgb(82, 82, 82);
+      width: 30px;
+      height: 30px;
+      border-radius: 50%;
+      cursor: pointer;
+    }
+  }
+
+  .form_new_car {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 10px 15px;
+    margin: 10px;
+
+    .content {
+      display: flex;
+      flex-direction: column;
+
+      label {
+        font-size: 13px;
+      }
+      input {
+        padding: 5px;
+        font-size: 13px;
+        border-radius: 3px;
+
+        border: 1px solid lightgrey;
+        outline: none;
+      }
+    }
+  }
+  footer {
+    padding-top: 10px;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    border-top: 1px solid #444;
+
+    input,
+    button {
+      padding: 10px 20px;
+      margin: 0px 3px;
+      border: none;
+      border-radius: 2px;
+      cursor: pointer;
+      color: white;
+      background-color: #02343fbd;
+    }
+    input:hover,
+    button:hover {
+      transform: translateZ(5px) scale(1.1);
+      background-color: #02343f;
+      border-radius: 3px;
+    }
+  }
+}
+</style>
