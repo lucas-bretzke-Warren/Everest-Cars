767273da981239a2148b66ce6c92fb98cfb407bf
Lucas Bretzke <lucas.bretzke@warren.com.br>
2022-07-12 11:24:31 -0300

    feat: import de TopBarHome e ListingOnCars

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <main>
-    <TopBarHome />
+    <TopBarHome/>
     <ListingOnCars />
   </main>
 </template>
@@ -12,7 +12,15 @@ export default {
   name: "HomeView",
   components: {
     TopBarHome,
-    ListingOnCars
+    ListingOnCars,
   },
 };
 </script>
+
+<style lang="less" scoped>
+main {
+  height: 100vh;
+  background-image: linear-gradient(to top, #30cfd0 0%, #330867 100%);
+  background-repeat: no-repeat;
+}
+</style>
