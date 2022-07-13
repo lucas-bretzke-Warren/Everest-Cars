0800a680c23cb8405e50a7ebf5a76eeaac4fc65b
Lucas Bretzke <lucas.bretzke@warren.com.br>
2022-07-13 01:24:06 -0300

    refactor: deleted unecessary code

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,14 @@
 <template>
   <main>
-    <TopBarHome/>
     <ListingOnCars />
   </main>
 </template>
 
 <script>
-import TopBarHome from "../components/TopBarHome.vue";
 import ListingOnCars from "../components/ListingOnCars.vue";
 export default {
   name: "HomeView",
   components: {
-    TopBarHome,
     ListingOnCars,
   },
 };
