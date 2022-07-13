221e80f302e4df57079a390001ae0dab6c09aa4a
Lucas Bretzke <lucas.bretzke@warren.com.br>
2022-07-13 01:32:35 -0300

    refactor: added component (ModalFrom),navBar,header, e method deleteCar

--- src/components/ListingOnCars.vue
+++ src/components/ListingOnCars.vue
@@ -1,59 +1,93 @@
 <template>
-  <div class="container-list">
-    <ul class="titles">
-      <li>Carro</li>
-      <li>Ano</li>
-    </ul>
-    <ul v-for="car in dataCars" :key="car.id">
-      <li>{{ car.nome }}</li>
-      <li>{{ car.ano }}</li>
-      <li><button class="btn-put">PUT</button></li>
-      <li><button class="btn-delete" @click="deleteCar()">DELETE</button></li>
-    </ul>
+  <div>
+    <nav>Lista de carros</nav>
+    <header><button @click="openFormNewCar()">POST</button></header>
+
+    <section class="container-list">
+      <ul class="titles">
+        <li>Carro</li>
+        <li>Ano</li>
+      </ul>
+      <ul v-for="car in dataCars" :key="car.id">
+        <li>{{ car.nome }}</li>
+        <li>{{ car.ano }}</li>
+        <li><button class="btn-put" @click="patchCar()">PUT</button></li>
+        <li>
+          <button class="btn-delete" @click="deleteCar(car.id)">DELETE</button>
+        </li>
+      </ul>
+      <ModalForm v-show="form_new_car" @closeModal="closeFormNewCar()" />
+      <!-- <section v-show="confirm_delete" class="modal-confirm-delete">
+        <h4>Tem certeza</h4>
+        <h4>que deseja deletar este item?</h4>
+        <div>
+          <button @click="closeModal()">Cancelar</button
+          ><button @click="deleteCar()">Sim</button>
+        </div>
+      </section> -->
+    </section>
   </div>
 </template>
 
 <script>
 import axios from "axios";
 
+import ModalForm from "../components/ModalForm.vue";
+
 export default {
   name: "ListingOnCars",
+  components: {
+    ModalForm,
+  },
   data() {
     return {
-      url: "https://my-json-server.typicode.com/willianWarren/backendAPI/cars",
+      url: "http://localhost:8080/api/cars",
       dataCars: [],
+      confirm_delete: false,
+      form_new_car: false,
     };
   },
   methods: {
+    openFormNewCar() {
+      this.form_new_car = true;
+    },
+    closeFormNewCar() {
+      this.form_new_car = false;
+      this.getCars();
+    },
+    openModal() {
+      this.confirm_delete = true;
+    },
+    closeModal() {
+      this.confirm_delete = false;
+    },
+
     async getCars() {
       try {
         const response = await axios.get(this.url);
-        this.dataCars = response.data;
+        this.dataCars = response.data.cars;
         console.log(this.dataCars);
       } catch (error) {
         console.log(error);
       }
     },
-    async teste() {
+
+    async patchCar() {
+      const editItemsTo = {
+        name: "Virou uma carroça",
+      };
       try {
-        const ddd = {
-          nome: "Corsa",
-          marca: "Fiat",
-          cor: "vermelho",
-          ano: "2001",
-        };
-        await axios.post(this.url, ddd);
-        console.log(this.dataCars);
+        await axios.patch(this.url, editItemsTo);
       } catch (error) {
         console.log(error);
       }
     },
-    async deleteCar() {
+
+    async deleteCar(id) {
       try {
-        await axios.delete(
-          "https://my-json-server.typicode.com/willianWarren/backendAPI/cars",
-          console.log(this.dataCars)
-        );
+        await axios.delete(`${this.url}/${id}`);
+        this.getCars();
+        this.confirm_delete = false;
       } catch (error) {
         console.log(error);
       }
@@ -66,7 +100,43 @@ export default {
 </script>
 
 <style lang="less" scoped>
+@teste: rgb(82, 82, 82);
 @border-color: rgba(105, 105, 105, 0.542);
+
+nav {
+  width: 100%;
+  height: 44px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  color: white;
+  background-color: @teste;
+  font-size: 17px;
+}
+header {
+  width: 100%;
+  height: 150px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  button {
+    padding: 7px 36px;
+
+    border: none;
+    border-radius: 3px;
+    color: white;
+    background-color: green;
+    cursor: pointer;
+  }
+  button:hover {
+    transition: 0.3s;
+    transform: translateZ(10px) scale(1.1);
+  }
+}
 
 .container-list {
   width: 770px;
@@ -123,6 +193,45 @@ export default {
       }
       .btn-delete:hover {
         transform: translateZ(10px) scale(1.1);
+      }
+    }
+  }
+  .modal-confirm-delete {
+    width: 300px;
+    height: 100px;
+
+    display: grid;
+    align-items: center;
+    justify-content: center;
+    border-radius: 5px;
+
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    right: -50%;
+    transform: translate(-50%, -50%);
+
+    background-color: white;
+    color: black;
+    box-shadow: 0px 0px 1px 1100px rgba(77, 77, 77, 0.151);
+    div {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+
+      button {
+        padding: 7px 13px;
+        margin: 0px 3px;
+        border: none;
+        border-radius: 3px;
+        cursor: pointer;
+        color: white;
+        background-color: #02343fd2;
+      }
+      button:hover {
+        transform: translateZ(5px) scale(1.1);
+        background-color: #02343f;
+        border-radius: 3px;
       }
     }
   }
